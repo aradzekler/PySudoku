a529c37a380209c621f3746323f2dc873060623b
aradzekler <mcspancy@gmail.com>
2020-09-01 18:02:30 +0300

    WIP

--- sudoku_gui.py
+++ sudoku_gui.py
@@ -1,6 +1,7 @@
 import pygame
 
 
+# class for dealing with the gui representation of the sudoku
 class GuiSquare:
     rows = 9
     cols = 9
@@ -14,19 +15,41 @@ class GuiSquare:
         self.height = height
         self.selected = False
 
-        def draw(self, win):
-            fnt = pygame.font.SysFont("comicsans", 40)
+    def draw(self, win):
+        fnt = pygame.font.SysFont("comicsans", 40)
 
-            gap = self.width / 9
-            x = self.col * gap
-            y = self.row * gap
+        gap = self.width / 9
+        x = self.col * gap
+        y = self.row * gap
 
-            if self.temp != 0 and self.value == 0:
-                text = fnt.render(str(self.temp), 1, (128, 128, 128))
-                win.blit(text, (x + 5, y + 5))
-            elif not (self.value == 0):
-                text = fnt.render(str(self.value), 1, (0, 0, 0))
-                win.blit(text, (x + (gap / 2 - text.get_width() / 2), y + (gap / 2 - text.get_height() / 2)))
+        if self.temp != 0 and self.value == 0:
+            text = fnt.render(str(self.temp), 1, (128, 128, 128))
+            win.blit(text, (x + 5, y + 5))
+        elif not (self.value == 0):
+            text = fnt.render(str(self.value), 1, (0, 0, 0))
+            win.blit(text, (x + (gap / 2 - text.get_width() / 2), y + (gap / 2 - text.get_height() / 2)))
 
-            if self.selected:
-                pygame.draw.rect(win, (255, 0, 0), (x, y, gap, gap), 3)
+        if self.selected:
+            pygame.draw.rect(win, (255, 0, 0), (x, y, gap, gap), 3)
+
+    def draw_change(self, win, g=True):
+        fnt = pygame.font.SysFont("comicsans", 40)
+
+        gap = self.width / 9
+        x = self.col * gap
+        y = self.row * gap
+
+        pygame.draw.rect(win, (255, 255, 255), (x, y, gap, gap), 0)
+
+        text = fnt.render(str(self.value), 1, (0, 0, 0))
+        win.blit(text, (x + (gap / 2 - text.get_width() / 2), y + (gap / 2 - text.get_height() / 2)))
+        if g:
+            pygame.draw.rect(win, (0, 255, 0), (x, y, gap, gap), 3)
+        else:
+            pygame.draw.rect(win, (255, 0, 0), (x, y, gap, gap), 3)
+
+    def set_val(self, val):
+        self.value = val
+
+    def set_temp(self, val):
+        self.temp = val
